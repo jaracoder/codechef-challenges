1d024d28aaa96c92c106533258b63a633f61fa07
jripolldev <juan.ripoll.93@gmail.com>
2019-04-05 19:46:03 +0200

    Primer proyecto ATM en codechef.

---
+++ codechef/ATM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codechef
+{
+    class ATM
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+
+            // dinero a retirar
+            int x = int.Parse(line[0]);
+
+            // balance en cuenta
+            double y = double.Parse(line[1], System.Globalization.CultureInfo.InvariantCulture);
+            
+            // comision
+            double comision = 0.50f;
+
+            // si no es multiplo de 5 o no hay balance en cuenta
+            if ( (x % 5 != 0) || ((x + comision) > y) )
+            {
+                Console.WriteLine(y);
+                return;
+            }
+            
+            // muestra balance despues de retirar dinero
+            double result = y - (x + comision);
+            Console.WriteLine(result.ToString("f2"));
+
+            Console.ReadLine();
+        }
+    }
+}
