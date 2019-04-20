4e2731d9133ee56108feaaac2049eb68f2b83171
jripolldev <juan.ripoll.93@gmail.com>
2019-04-20 08:08:02 +0200

    - Añade AddTwoNumbers.cs

---
+++ codechef/practices/AddTwoNumbers.cs
@@ -0,0 +1,21 @@
+﻿// jripoll.dev
+
+using System;
+
+class FLOW001
+{
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+        string[] ab;
+
+        for (int i = 0; i < t; i++)
+        {
+            ab = Console.ReadLine().Split(' ');
+            Console.WriteLine(
+                int.Parse(ab[0]) + int.Parse(ab[1])
+            );
+        }
+    }
+}
+
