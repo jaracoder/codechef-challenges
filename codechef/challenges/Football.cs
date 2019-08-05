c897fd93dc4636723e4d577ed21df5bae17eee7f
Juan Antonio Ripoll <juan.ripoll.93@gmail.com>
2019-08-05 09:26:10 +0200

    Football.cs

---
+++ codechef/challenges/Football.cs
@@ -0,0 +1,41 @@
+﻿// jripoll.dev - 05/08/2019
+// Football.cs
+// Problem Code: MSNSADM1
+
+using System;
+
+class Football
+{
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+
+        int i = 0;
+        for (; i < t; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] goals = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int[] fouls = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            int c = 0;
+            int max = 0;
+            for (; c < n; c++)
+            {
+                int goal = goals[c];
+                int foul = fouls[c];
+                int result = (goal * 20) - (foul * 10);
+
+                if (result > max)
+                    max = result;
+            }
+
+            Console.WriteLine(max);
+        }
+           
+#if DEBUG
+        Console.ReadLine();
+#endif
+    }
+}
+
